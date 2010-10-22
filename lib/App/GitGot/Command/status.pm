da0576dd80f5007d7d5813bee22a6d0f0525f29b
John SJ Anderson <genehack@genehack.org>
2010-10-22 10:14:50 -0400

    refactor to enable support for more than just git
    none of that actually works yet, of course.

--- lib/App/GitGot/Command/status.pm
+++ lib/App/GitGot/Command/status.pm
@@ -14,28 +14,49 @@ sub execute {
  REPO: for my $entry ( @{ $self->repos }) {
     my( $name , $path ) = @{$entry}{qw/name path/};
 
+    my $msg = sprintf "%3d) %-25s : " , $entry->{number} , $entry->{name};
+
     unless ( -d $path ) {
-      say "Repo $name does not exist";
+      say "${msg}ERROR: repo '$name' does not exist"
+        if $self->verbose;
       next REPO;
     }
 
-    if ( -d "$path/.git" ) {
-      my( $o , $e ) = capture { system( "cd $path && git status" ) };
+    print $msg;
 
-      my $msg = sprintf "%3d) %-25s : " , $entry->{number} , $entry->{name};
+    my $fxn = undef;
 
-      if ( $o =~ /^nothing to commit/m and ! $e ) {
-        if ( $o =~ /Your branch is ahead .*? by (\d+) / ) {
-          $msg .= "Ahead by $1";
-        }
-        else { $msg .= 'OK' }
-      }
-      else { $msg .= 'Dirty' }
-
-      say $msg;
-
-      print "$o$e" if ( $self->verbose );
+    given( $entry->{type} ) {
+      when( 'git' ) { $fxn = 'git_status' }
+      ### FIXME      when( 'svn' ) { $fxn = 'svn_status' }
+      default { say "ERROR: repo type '$_' not supported" }
     }
+
+    say $self->$fxn( $entry ) if ( $fxn );
+  }
+}
+
+sub git_status {
+  my( $self , $entry ) = @_
+    or die "Need entry";
+
+  my $path = $entry->{path};
+
+  my $msg;
+
+  if ( -d "$path/.git" ) {
+    my( $o , $e ) = capture { system( "cd $path && git status" ) };
+
+    if ( $o =~ /^nothing to commit/m and ! $e ) {
+      if ( $o =~ /Your branch is ahead .*? by (\d+) / ) {
+        $msg .= "Ahead by $1";
+      }
+      else {$msg .= 'OK' }
+    }
+    elsif ( $e ) { $msg .= 'ERROR' }
+    else { $msg .= 'Dirty' }
+
+    return ( $self->verbose ) ? "$msg\n$o$e" : $msg;
   }
 }
 
