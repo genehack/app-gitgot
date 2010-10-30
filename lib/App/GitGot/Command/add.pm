7dba6ee882b75596921163b7ee6d987e24191c73
John SJ Anderson <genehack@genehack.org>
2010-10-30 18:25:00 -0400

    add 'defaults' option to 'add' cmd

--- lib/App/GitGot/Command/add.pm
+++ lib/App/GitGot/Command/add.pm
@@ -9,16 +9,62 @@ use Cwd;
 use Term::ReadLine;
 use Term::ReadLine::Perl;    # this is listed so we pull it in as a dep
 
+has 'defaults' => (
+  is          => 'rw',
+  isa         => 'Bool',
+  cmd_aliases => 'D',
+  traits      => [qw/ Getopt /],
+);
+
 sub execute {
   my ( $self, $opt, $args ) = @_;
 
-  my $new_entry = _build_new_entry_from_user_input();
+  my $new_entry = $self->build_new_entry_from_user_input();
 
   # this will exit if the new_entry duplicates an existing repo in the config
   $self->check_for_dupe_entries($new_entry);
 
   push @{ $self->config }, $new_entry;
   $self->write_config;
+}
+
+sub build_new_entry_from_user_input {
+  my ($self) = @_;
+
+  my ( $repo, $name, $type, $tags, $path );
+
+  if ( -e '.git' ) {
+    ( $repo, $name, $type ) = _init_for_git();
+  }
+  else {
+    say "ERROR: Non-git repos not supported at this time.";
+    exit;
+  }
+
+  if ( $self->defaults ) {
+    die "ERROR: Couldn't determine name"      unless $name;
+    die "ERROR: Couldn't determine repo path" unless $repo;
+    die "ERROR: Couldn't determine repo type" unless $type;
+    $path = getcwd or die "ERROR: Couldn't determine path";
+  }
+  else {
+    my $term = Term::ReadLine->new('gitgot');
+    $name = $term->readline( 'Name: ', $name );
+    $repo = $term->readline( ' URL: ', $repo );
+    $path = $term->readline( 'Path: ', getcwd );
+    $tags = $term->readline( 'Tags: ', $tags );
+  }
+
+  my $new_entry = {
+    repo => $repo,
+    name => $name,
+    type => $type,
+    path => $path,
+  };
+
+  $new_entry->{tags} = $tags if $tags;
+
+  return $new_entry;
 }
 
 sub check_for_dupe_entries {
@@ -33,35 +79,6 @@ REPO: foreach my $entry ( @{ $self->config } ) {
 "ERROR: Not adding entry for '$entry->{name}'; exact duplicate already exists.";
     exit;
   }
-}
-
-sub _build_new_entry_from_user_input {
-  my ( $repo, $name, $type, $tags, $path );
-
-  if ( -e '.git' ) {
-    ( $repo, $name, $type ) = _init_for_git();
-  }
-  else {
-    say "ERROR: Non-git repos not supported at this time.";
-    exit;
-  }
-
-  my $term = Term::ReadLine->new('gitgot');
-  $name = $term->readline( 'Name: ', $name );
-  $repo = $term->readline( ' URL: ', $repo );
-  $path = $term->readline( 'Path: ', getcwd );
-  $tags = $term->readline( 'Tags: ', $tags );
-
-  my $new_entry = {
-    repo => $repo,
-    name => $name,
-    type => $type,
-    path => $path,
-  };
-
-  $new_entry->{tags} = $tags if $tags;
-
-  return $new_entry;
 }
 
 sub _init_for_git {
