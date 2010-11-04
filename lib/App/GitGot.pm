eded782d9aa8ed50cc2f775c8465827b24666301
John SJ Anderson <genehack@genehack.org>
2010-11-04 06:00:43 -0400

    convert to dist.ini ABSTRACT

--- lib/App/GitGot.pm
+++ lib/App/GitGot.pm
@@ -1,13 +1,6 @@
 package App::GitGot;
 use Moose;
 extends 'MooseX::App::Cmd';
-
+# ABSTRACT: A tool to make it easier to manage multiple git repositories.
 use version; our $VERSION = qv("v0.01");
-
 1;
-
-__END__
-
-=head1 NAME
-
-App::GitGot - A tool to make it easier to manage multiple git repositories.
