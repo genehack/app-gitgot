e03016f8ca6fd5883720e619884593d4b23a68c8
John SJ Anderson <genehack@genehack.org>
2010-11-09 23:27:36 -0500

    add 'chdir' command

---
+++ lib/App/GitGot/Command/chdir.pm
@@ -0,0 +1,24 @@
+package App::GitGot::Command::chdir;
+# ABSTRACT: fork a subshell in a selected project
+
+use Moose;
+extends 'App::GitGot::Command';
+use 5.010;
+
+sub command_names { qw/ chdir cd / }
+
+sub _execute {
+  my( $self, $opt, $args ) = @_;
+
+  unless ( $self->active_repos and $self->active_repos == 1 ) {
+    say "You need to select a single repo";
+    exit;
+  }
+
+  my( $repo ) = $self->active_repos;
+
+  chdir $repo->path;
+  exec $ENV{SHELL};
+}
+
+1;
