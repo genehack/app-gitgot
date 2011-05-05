7980b5b2ca024c5e52ba27e54e6fa161177f4750
John SJ Anderson <genehack@genehack.org>
2011-05-04 23:22:59 -0400

    add 'update_status' command
    two great tastes that taste great together.

---
+++ lib/App/GitGot/Command/update_status.pm
@@ -0,0 +1,21 @@
+package App::GitGot::Command::update_status;
+# ABSTRACT: update managed repositories then display their status
+
+use Moose;
+extends 'App::GitGot::Command';
+use 5.010;
+
+sub command_names { qw/ update_status upst / }
+
+sub _execute {
+  my ( $self, $opt, $args ) = @_;
+
+  say "UPDATE";
+  $self->_update( $self->active_repos );
+
+  say "\nSTATUS";
+  $self->_status( $self->active_repos );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
