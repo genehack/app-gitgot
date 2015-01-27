2ec39b8012873f37430877d9ec5240d47b0e4c51
John SJ Anderson <genehack@genehack.org>
2015-01-26 22:38:49 -0800

    Whitespace cleanups and tweaks

--- lib/App/GitGot/Command/do.pm
+++ lib/App/GitGot/Command/do.pm
@@ -13,10 +13,10 @@ use File::chdir;
 use Capture::Tiny qw/ capture_stdout /;
 
 has 'with_repo' => (
-  is          => 'ro',
-  isa         => 'Bool',
-  default     => 0,
-  traits      => [qw/ Getopt /],
+  is            => 'ro',
+  isa           => 'Bool',
+  default       => 0,
+  traits        => [qw/ Getopt /],
   documentation => 'prepend all output lines with the repo name',
 );
 
@@ -31,23 +31,22 @@ sub _execute {
 }
 
 sub _run_in_repo {
-    my( $self, $repo, $cmd ) = @_;
+  my( $self, $repo, $cmd ) = @_;
 
-    if ( not -d $repo->path ) {
-        printf "repo %s: no repository found at path '%s'\n", 
-            $repo->label, $repo->path;
-        return;
-    }
+  if ( not -d $repo->path ) {
+    printf "repo %s: no repository found at path '%s'\n",
+      $repo->label, $repo->path;
+    return;
+  }
 
-    say "\n## repo ", $repo->label, "\n" unless $self->with_repo;
+  say "\n## repo ", $repo->label, "\n" unless $self->with_repo;
 
-    my $prefix = $self->with_repo ? $repo->label . ': ' : '';
+  my $prefix = $self->with_repo ? $repo->label . ': ' : '';
 
-    say $prefix, $_ for split "\n", capture_stdout {
-        $CWD = $repo->path;
-        system @$cmd;
-    };
-;
+  say $prefix, $_ for split "\n", capture_stdout {
+    $CWD = $repo->path;
+    system @$cmd;
+  };
 }
 
 __PACKAGE__->meta->make_immutable;
