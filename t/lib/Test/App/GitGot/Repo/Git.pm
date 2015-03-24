9812a2a13b7a1c18cd377ab014d8b2e634b18955
John SJ Anderson <genehack@genehack.org>
2015-03-23 22:31:15 -0700

    Tweak error message detection [#53]

--- t/lib/Test/App/GitGot/Repo/Git.pm
+++ t/lib/Test/App/GitGot/Repo/Git.pm
@@ -21,7 +21,7 @@ sub test_current_branch :Tests(3) {
   my $test = shift;
 
   dies_ok { $test->{full}->current_branch } 'will die';
-  like( $@ , qr/Can't locate directory/ , 'expected error message' );
+  like( $@ , qr/(?:Can't locate|Failed to change) directory/ , 'expected error message' );
 
   is( $test->{min}->current_branch , 'master' , 'expected answer' );
 }
@@ -30,7 +30,7 @@ sub test_current_remote_branch :Tests(3) {
   my $test = shift;
 
   dies_ok { $test->{full}->current_branch } 'will die';
-  like( $@ , qr/Can't locate directory/ , 'expected error message' );
+  like( $@ , qr/(?:Can't locate|Failed to change) directory/ , 'expected error message' );
 
   is( $test->{min}->current_remote_branch , 0 , 'get 0 without real remote' );
 }
