1bcff250665d9db1e068638fa4f9236164f4bb0f
John SJ Anderson <john@genehack.org>
2017-06-22 15:36:45 -0400

    Replace heredoc with print()s
    Also update Changes; add ABSTRACT to milk.pm

--- lib/App/GitGot/Command/milk.pm
+++ lib/App/GitGot/Command/milk.pm
@@ -2,6 +2,7 @@ package App::GitGot::Command::milk;
 
 use 5.014;
 
+# ABSTRACT: well, do you?
 use App::GitGot -command;
 
 use Moo;
@@ -11,19 +12,18 @@ use namespace::autoclean;
 sub command_names { qw/ milk / }
 
 sub _execute {
-	# Doesn't use 'cowsay' in case it's not installed
-	my $msg <<EOT;
- __________
-< got milk? >
- ----------
-		\   ^__^
-		 \  (oo)\_______
-			(__)\       )\/\
-				||----w |
-				||	   ||
-EOT
-	print $msg;
+        # Doesn't use 'cowsay' in case it's not installed
+  print " ___________\n";
+  print "< got milk? >\n";
+  print " -----------\n";
+  print "        \\   ^__^\n";
+  print "         \\  (oo)\\_______\n";
+  print "            (__)\\       )\\/\\ \n";
+  print "                ||----w |\n";
+  print "                ||     ||\n";
+  print "\n";
 }
 
-1;
 
+
+1;
